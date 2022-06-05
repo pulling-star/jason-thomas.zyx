c30e003c7cc513b9aec3180ff85eaf2051429a12
pulling-star <milanjackson426@gmail.com>
2022-06-05 11:24:25 +1000

    Changes to DB

--- init_db.py
+++ init_db.py
@@ -1,3 +1,5 @@
+#!/usr/bin/env python3
+
 from app import *
 from contextlib import closing
 
--- schema.sql
+++ schema.sql
@@ -1,7 +1,15 @@
-drop table if exists entries;
-create table entries (
+CREATE TABLE IF NOT EXISTS entries (
   id integer primary key autoincrement,
   entryDate date not null,
   title text not null,
   text text not null
-);
+)
+
+CREATE TABLE IF NOT EXISTS "articles" (
+	"id"	integer PRIMARY KEY AUTOINCREMENT,
+	"publishDate"	date NOT NULL,
+	"link"	text NOT NULL,
+	"title"	text NOT NULL,
+	"text"	text NOT NULL,
+	"interactive"	NUMERIC NOT NULL DEFAULT 0
+)
