87994955289920cd23d3c8fb14c025b121f6b64b
pulling-star <milanjackson426@gmail.com>
2022-06-16 12:04:08 +1000

    I think I need to make all links https

--- static/projects/SBS_dataviz/vic_fire_map/js/map.js
+++ static/projects/SBS_dataviz/vic_fire_map/js/map.js
@@ -7,7 +7,7 @@ var map = L.map('map',{
 	scrollWheelZoom: false
 });
 
-L.tileLayer('http://server.arcgisonline.com/ArcGIS/rest/services/World_Topo_Map/MapServer/tile/{z}/{y}/{x}', {
+L.tileLayer('https://server.arcgisonline.com/ArcGIS/rest/services/World_Topo_Map/MapServer/tile/{z}/{y}/{x}', {
 	attribution: 'Tiles &copy; Esri &mdash; Esri, DeLorme, NAVTEQ, TomTom, Intermap, iPC, USGS, FAO, NPS, NRCAN, GeoBase, Kadaster NL, Ordnance Survey, Esri Japan, METI, Esri China (Hong Kong), and the GIS User Community',
 	maxZoom: 14,
 	minZoom: 9
